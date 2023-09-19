9feeee49694c235df157703b1f0f9b6ef98b5ad7
eschala <eduar.chala28@outlook.com>
2023-09-19 12:07:52 -0500

    A

--- JavaScript/T1_JavaScript.js
+++ JavaScript/T1_JavaScript.js
@@ -1,1 +1,34 @@
-alert("Bueno, me voy poniendo lo mio porque con eso no fio");
+
+
+
+function mostrarDatos(){
+
+
+    nameInput=prompt("Nombre");
+    ageInput=prompt("Edad");
+    cityInput=prompt("Ciudad");
+    document.write("<br>");
+
+    document.write(`A ${nameInput}B ${ageInput}C ${cityInput}`);
+
+
+
+    if(ageInput>18){
+
+        alert("bienvenido");
+
+    }
+    else{
+        alert("devuelvase mijo");
+
+
+    }
+
+    document.write("<br>");
+    document.write(document.getElementById("input-name").value);
+    document.write("<br>");
+    document.write(document.getElementById("input-age").value);
+    document.write("<br>");
+    document.write(document.getElementById("input-city").value);
+
+}
