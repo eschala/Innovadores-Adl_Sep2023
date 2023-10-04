cc7e184427367a23034b9ed10afee0308a45cee7
eschala <eduar.chala28@outlook.com>
2023-10-04 18:10:30 -0500

    Edit 6

--- Z_Innovadores ADL - Introducción a JavaScript/Taller_2_03_Oct_2023/JavaScript/Taller_2_DOM_Script.js
+++ Z_Innovadores ADL - Introducción a JavaScript/Taller_2_03_Oct_2023/JavaScript/Taller_2_DOM_Script.js
@@ -14,33 +14,84 @@ const contenedores_ = document.querySelectorAll(".checkbox-container");
 var activar_Checkbox, desactivar_Checkbox;
 const stylesForCheckbox = document.querySelector(".checkbox_input");
 const contenedores_ = document.querySelectorAll(".checkbox-container");
+const bodyStyles = document.querySelector(".body_page");
+
+const bodyIdSelect = document.querySelector("#body__");
 
 const activateCheckbox = () => {
-  /* checkbox funcion */
+  alert("probando");
+  /* checkbox funcion activar/desactivar*/
+
   for (i = 0; i < contenedores_.length; i++) {
     contenedores_[i].classList.toggle("activateCheckbox");
 
-    if (stylesForCheckbox.checked == false) {
-      alert("Checkbox Activado");
-
-
-
-
-
-    } else if (stylesForCheckbox.checked == true) {
-      alert("Checkbox Desactivado");
-
-      
-
-
+    {
+      if (stylesForCheckbox.checked == true) {
+        applyStylesCheckbox();
+      } else if (stylesForCheckbox.checked == false) {
+        putOffStylesForCheckbox();
+      }
     }
-
-
-
+    {
+      /* if(){}else
+      if(){}else{} */
+    }
   }
-
-
-
-  
 };
 
+applyStylesCheckbox = () => {
+  /* aplicar estilo */
+  alert("Modo oscuro Activado");
+  bodyStyles.style.backgroundColor = "rgb(7, 2, 48)";
+};
+putOffStylesForCheckbox = () => {
+  /* desactivar estilo estilo */
+  alert("Modo oscuro Desactivado");
+  bodyStyles.style.backgroundColor = "azure";
+};
+
+const button1 =
+  document.querySelector(
+    ".button-1"
+  ); /* boton para cambiar el fondo y la fuente */
+const button2 =
+  document.querySelector(
+    ".button-2"
+  ); /* boton para cambiar el tamño de la fuente y la fuente */
+const button3 =
+  document.querySelector(".button-3"); /* boton para desahacer cambios*/
+
+changeBackground_Font = () => {
+  /*   alert("boton1"); */
+  for (i = 0; i < contenedores_.length; i++) {
+    contenedores_[i].classList.toggle("changeBackground_Font");
+
+/*  */
+
+    bodyIdSelect.style.backgroundColor = "darkblue";
+    (bodyIdSelect.style.fontFamily = "Poppins"), "sans-serif";
+    bodyIdSelect.style.color = "violet";
+  }
+};
+
+changeSizeFont_FamilyFont = () => {
+  /*   alert("boton2"); */
+  for (i = 0; i < contenedores_.length; i++) {
+    contenedores_[i].classList.toggle("changeSizeFont_FamilyFont");
+
+    bodyIdSelect.style.fontSize = "50px";
+    (bodyIdSelect.style.fontFamily = "Roboto"), "sans-serif";
+    bodyIdSelect.style.color = "blueviolet";
+  }
+};
+
+restoreChanges = () => {
+  /*   alert("boton3"); */
+  for (i = 0; i < contenedores_.length; i++) {
+    contenedores_[i].classList.toggle("restoreChanges");
+
+    bodyIdSelect.style.backgroundColor = "azure";
+  }
+
+  /* bodyStyles[i].undo; */
+};
