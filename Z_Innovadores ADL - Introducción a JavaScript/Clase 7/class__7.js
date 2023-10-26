5f8fb888fd4069507f63f0fc6860d4e46584539d
eschala <eduar.chala28@outlook.com>
2023-10-26 17:48:39 -0500

    Clase 8

--- Z_Innovadores ADL - Introducción a JavaScript/Clase 7/class__7.js
+++ Z_Innovadores ADL - Introducción a JavaScript/Clase 7/class__7.js
@@ -2,7 +2,22 @@ const text_User = document.querySelector(".text");
 const info_User = document.querySelector(".info");
 
 
+
+const onKeyEnter=(event)=>{
+
+let keyPress =event.key;
+
+
+if(keyPress==="Enter"){
+
+    alert("xD ha presionado enter");
+}
+
+
+}
+
 const key_Pressed = (event)=>{
+    onKeyEnter(event);
 
     let key = event.key;
     info_User.innerHTML="la tecla presionada es : "+key;
