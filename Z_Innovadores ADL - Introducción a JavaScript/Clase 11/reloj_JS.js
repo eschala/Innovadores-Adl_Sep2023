6fd94ed908e653edf9ee80a213b2bec02764d06d
eschala <eduar.chala28@outlook.com>
2023-11-23 17:36:21 -0500

    Clase 11

---
+++ Z_Innovadores ADL - Introducción a JavaScript/Clase 11/reloj_JS.js
@@ -0,0 +1,11 @@
+const fecha_Reloj = new Date();
+
+const hour_Date = fecha_Reloj.getHours();
+const minutes_Date = fecha_Reloj.getMinutes();
+const seconds_Date = fecha_Reloj.getSeconds();
+
+
+
+const get_Hour=()=>{
+    
+}
