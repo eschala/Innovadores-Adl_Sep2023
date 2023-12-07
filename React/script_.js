aa79963aef777cbd30a1196aaa417fdfa6ea09e0
eschala <eduar.chala28@outlook.com>
2023-12-07 13:47:05 -0500

    A

--- React/script_.js
+++ React/script_.js
@@ -1,31 +1,33 @@
-const samir=["hola","Como estas?", "Me alegra Mucho", "A Dios, me voy"];
+const samir = ["hola", "Como estas?", "Me alegra Mucho", "A Dios, me voy"];
 
-
-
-for(let i=0; i<samir.length;i++){
-
-    document.write(samir[i]+"<br>");
-
-
-
+for (let i = 0; i < samir.length; i++) {
+  document.write(samir[i] + "<br>");
 }
-
 
 const body_Element = document.querySelector("body");
+body_Element.style.height = "800px";
 
-var contar=0;
+var contar = 0;
 
-var operar=0;
-const contarClicBody = (operar)=>{
+var operar = 0;
 
-    contar++;
-    operar=contar;
-
-
-
+const mostrarMsg=()=>
+{
+alert(operar);}
 
 
-const titulo_ = document.querySelector(".titulo");
-titulo_.innerHTML=operar;
-}
-body_Element.addEventListener("click",contarClicBody);
+const contarClicBody = (operar) => {
+  contar++;
+  operar = contar;
+
+  const titulo_ = document.querySelector(".titulo");
+  titulo_.innerHTML = operar;
+
+  return operar;
+
+};
+
+
+
+
+body_Element.addEventListener("click", contarClicBody);
